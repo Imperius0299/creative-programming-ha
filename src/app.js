8d32dfa5347a0d5cbb4472fb2822ab19e5bc67ac
Alexander Storbeck <AlexSt2768@gmail.com>
2020-12-29 11:46:37 +0100

    added tfjs and express, initialized npm

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const tfjs = require('@tensorflow/tfjs')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () =>{
+    console.log(`App is listening on http://localhost:${port}`)
+})
